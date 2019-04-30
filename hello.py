d435fb0cba521afefc63cf02382c7b9aff319156
Marinho Kraemer <malmeida@newfields.com>
2019-04-30 10:04:16 -0300

    base template

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect, flash
 app = Flask(__name__)
 
 #TEMPLATE ================================
@@ -9,29 +9,31 @@ app = Flask(__name__)
 def hello(name = None):
     return render_template('hello.html', name=name)
 
-@app.route('/')
-def index():
-    return url_for('show_user_profile', username = 'richard')
-
 #método GET e POST 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return "User %s logged in" % request.form['username']
-    return render_template('login.html')
+        if valid_login(request.form['username'], request.form['password']):
+            flash("Succesfully logged in")
+            return redirect(url_for('welcome', username = request.form.get('username')))
+        else:
+            error = 'Incorret username and password'
+    return render_template('login.html', error = error)
 
-@app.route('/username/<username>')
-def show_user_profile(username):
-    #show the user profile for that user
-    return "User %s visited" % username
+def valid_login(username, password):
+    if username == password:
+        return True
+    else:
+        return False
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    #show the post with the give id, the id must be an integeger
-    return "Post %d" % post_id
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', '5000'))
     app.debug = True
+    app.secret_key = 'SuperSecretKey'
     app.run(host = host, port = port)
